1ce7066256bab6319f82f3d4ebadcd9d92043263
crislaine <murilobiss@gmail.com>
2022-08-09 21:20:43 -0300

    feat: fake_web_events lib by André Sionek feature tested with Kinesis

---
+++ how_fake_events.py
@@ -0,0 +1,21 @@
+import boto3
+import json
+from fake_web_events import Simulation
+
+client = boto3.client('firehose')
+
+def put_record(event):
+    data = json.dumps(event) + "\n"
+    response = client.put_record(
+        DeliveryStreamName='PUT-S3-gZSeH',
+        Record={"Data": data}
+    )
+    print(event)
+    return response
+
+
+simulation = Simulation(user_pool_size=100, sessions_per_day=100000)
+events = simulation.run(duration_seconds=3000)
+
+for event in events:
+    put_record(event)
