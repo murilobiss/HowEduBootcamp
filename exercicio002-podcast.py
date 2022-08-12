fd0facb6c65bb54cc6bea03d7e9e7554eb7a1103
crislaine <murilobiss@gmail.com>
2022-08-12 14:49:05 -0300

    fix: Changed names between variables

--- exercicio002-podcast.py
+++ exercicio002-podcast.py
@@ -1,23 +1,18 @@
-#%%
 import requests
 from bs4 import BeautifulSoup as bs
 import logging
 import pandas as pd
 
-#%%
 url = 'https://portalcafebrasil.com.br/todos/podcasts/page/{}/?ajax=true'
 
-#%%
 ret = requests.get(url)
 soup = bs(ret.text)
 
-#%%
 def get_podcast(url):
     ret = requests.get(url)
     soup = bs(ret.text)
     return soup.find_all('h5')
 
-#%%
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
@@ -26,7 +21,6 @@ ch = logging.StreamHandler()
 ch.setFormatter(formatter)
 log.addHandler(ch)
 
-#%%
 i = 1
 lst_podcast = []
 lst_get = get_podcast(url.format(i))
@@ -37,12 +31,9 @@ while len(lst_get) > 0:
     lst_get = get_podcast(url.format(i))
     log.debug(f"Coletado {len(lst_get)} episódios do link: {url.format(i)}")
 
-#%%
 df = pd.DataFrame(columns=['nome','link'])
 
-# %%
 for item in lst_podcast:
     df.loc[df.shape[0]] = [item.text, item.a['href']]
 
-# %%
 df.to_csv('banco_de_podcasts.csv', sep=',', index=False)
